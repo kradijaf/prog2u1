418ee0a9c4db3ff68829f6767bf000db48b29214
Andrej Šmelko <andrej.smelko@natur.cuni.cz>
2023-03-17 16:48:47 +0100

    slovniky objektov namiesto listov objektov, triedy majú viac atribútov

--- part1.py
+++ part1.py
@@ -1,15 +1,24 @@
 # Prvá časť, načítanie dát, vytvorenie tried, vytvorenie objektov
 
+# To Do:
+# private metódy
+# pridať try except 
+# komentáre
+
 import csv
 
 class Stop():
     
-    def __init__(self,stop_id,stopName):
+    def __init__(self, stop_id, stop_name, stop_lat, stop_lon):
         self.__id = stop_id
-        self.__name = stopName
+        self.__name = stop_name
+        self.__stopLat = stop_lat
+        self.__stopLon = stop_lon
+        self.__refToSTs = []
 
     def __str__(self):
-        return 'id: ' + str(self.__id) + ', name: ' + str(self.__name)
+        return 'id: ' + str(self.__id) + ', name: ' + str(self.__name) + ', stopLat: ' + str(self.__stopLat) \
+                + ', stopLon: ' + str(self.__stopLon) + ', refToSTs: ' + str(self.__refToSTs)
     
     @property
     def id(self):
@@ -19,18 +28,33 @@ class Stop():
     def name(self):
         return self.__name
     
-    @id.setter
-    def id(self, id):
-        self.__id = id
+    @property
+    def refToSTs(self):
+        return self.__refToSTs
+    
+    @property
+    def stopLat(self):
+        return self.__stopLat
+
+    @property
+    def stopLon(self):
+        return self.__stopLon
+
+    @refToSTs.setter
+    def refToSTs(self, refToSTs):
+        self.__refToSTs = refToSTs
 
 class StopTime():
 
-    def __init__(self,stop_id,trip_id):
+    def __init__(self, trip_id, arrival_time, departure_time, stop_id):
         self.__refToStop = stop_id
         self.__refToTrip = trip_id
+        self.__arrivalTime = arrival_time
+        self.__departureTime = departure_time
 
     def __str__(self):
-        return 'refToStop: ' + self.__refToStop + ', refToTrip: ' + self.__refToTrip
+        return 'refToStop: ' + self.__refToStop + ', refToTrip: ' + self.__refToTrip + ', arrivalTime: ' \
+                + str(self.__arrivalTime) + ', departureTime: ' + str(self.__departureTime)
     
     @property
     def refToStop(self):
@@ -40,6 +64,14 @@ class StopTime():
     def refToTrip(self):
         return self.__refToTrip
     
+    @property
+    def arrivalTime(self):
+        return self.__arrivalTime
+    
+    @property
+    def departureTime(self):
+        return self.__departureTime
+
     @refToStop.setter
     def refToStop(self, refToStop):
         self.__refToStop = refToStop
@@ -50,12 +82,13 @@ class StopTime():
 
 class Trip():
     
-    def __init__(self,trip_id,route_id):
+    def __init__(self, trip_id, route_id, trip_headsign):
         self.__refToSTs = trip_id
         self.__refToRoute = route_id
+        self.__tripHeadsign = trip_headsign
 
     def __str__(self):
-        return 'refToRoute: ' + self.__refToRoute + ', refToSTs: ' + self.__refToSTs
+        return 'refToRoute: ' + self.__refToRoute + ', refToSTs: ' + self.__refToSTs + ', tripHeadSign: ' + str(self.__tripHeadsign)
 
     @property
     def refToSTs(self):
@@ -64,6 +97,10 @@ class Trip():
     @property
     def refToRoute(self):
         return self.__refToRoute
+    
+    @property
+    def tripHeadsign(self):
+        return self.__tripHeadsign
     
     @refToSTs.setter
     def refToSTs(self, refToSTs):
@@ -75,12 +112,13 @@ class Trip():
 
 class Route():
 
-    def __init__(self,route_id,routeShortName):
+    def __init__(self, route_id, route_short_name, route_long_name):
         self.__refToTrips = route_id
-        self.__name = routeShortName
+        self.__name = route_short_name
+        self.__routeLongName = route_long_name
 
     def __str__(self):
-        return 'refToTrips: ' + self.__refToTrips + ', name: ' + self.__name
+        return 'refToTrips: ' + self.__refToTrips + ', name: ' + self.__name + ', routeLongName: ' + str(self.__routeLongName)
     
     @property
     def refToTrips(self):
@@ -90,44 +128,70 @@ class Route():
     def name(self):
         return self.__name
     
+    @property
+    def routeLongName(self):
+        return self.__routeLongName
+    
     @refToTrips.setter
     def refToTrips(self, refToTrips):
         self.__refToTrips = refToTrips
 
-# open files
-def createObjects(stopsFile,stopTimesFile,tripsFile,routesFile): # docstringy a open upraviť
-    with open(stopsFile, encoding = "utf-8", newline = "") as sp, \
-        open(stopTimesFile, encoding = "utf-8", newline = "") as st, \
-        open(tripsFile, encoding = "utf-8", newline = "") as tr, \
-        open(routesFile, encoding = "utf-8", newline = "") as rt:
+# open files, create dictionaries of objects
+def createObjects() -> tuple[dict, dict, dict, dict]:#(stopsFile,stopTimesFile,tripsFile,routesFile): # docstringy a open upraviť
+    """Creates 4 dictionaries, stops, stopTimes, trips, routes:
+    
+    1. key: (stop_id); value: (class Stops object): id, name, stopLat, stopLon, refToSTs \n 
+    2. key: (stop_id, trip_id); value: (class StopTime object): refToStop, refToTrip, arrivalTime, departureTime \n 
+    3. key: (route_id,trip_id); value: (class Trip object): refToSTs, refToRoute, tripHeadsign \n 
+    4. key: (route_id); value: (class Route object): refToTrips, name, routeLongName \n 
+    
+    Object atributes named "ref..." are supposed to be modified, they do NOT represent references to other objects at the time of their initialization"""
+    #with open(stopsFile, encoding = "utf-8", newline = "") as sp, \
+    #    open(stopTimesFile, encoding = "utf-8", newline = "") as st, \
+    #    open(tripsFile, encoding = "utf-8", newline = "") as tr, \
+    #    open(routesFile, encoding = "utf-8", newline = "") as rt:
 
     # ak sú súbory v inej zložke, do uvodzoviek dajte adresu
-    #with open(r"C:\Users\andre\Desktop\Prog_python\stops.txt", encoding = "utf-8", newline = "") as sp, \
-    #    open(r"C:\Users\andre\Desktop\Prog_python\stop_times.txt", encoding = "utf-8", newline = "") as st, \
-    #    open(r"C:\Users\andre\Desktop\Prog_python\trips.txt", encoding = "utf-8", newline = "") as tr, \
-    #    open(r"C:\Users\andre\Desktop\Prog_python\routes.txt", encoding = "utf-8", newline = "") as rt:
+    with open(r"C:\Users\andre\Desktop\Prog_python\stops.txt", encoding = "utf-8", newline = "") as sp, \
+        open(r"C:\Users\andre\Desktop\Prog_python\stop_times.txt", encoding = "utf-8", newline = "") as st, \
+        open(r"C:\Users\andre\Desktop\Prog_python\trips.txt", encoding = "utf-8", newline = "") as tr, \
+        open(r"C:\Users\andre\Desktop\Prog_python\routes.txt", encoding = "utf-8", newline = "") as rt:
         
 
         reader_sp = csv.reader(sp, delimiter=",")
         reader_st = csv.reader(st, delimiter=",")
         reader_tr = csv.reader(tr, delimiter=",")
         reader_rt = csv.reader(rt, delimiter=",")
+        
+        stops = {}
+        for idx, spLine in enumerate(reader_sp):
+            if idx == 0:
+                continue
+            stops[(spLine[0])] = Stop(spLine[0],spLine[1],spLine[2],spLine[3])
 
-        # list comprehension - vytvorí listy objektov
-        stops = []
-        stops = [Stop(spLine[0],spLine[1]) for idx, spLine in enumerate(reader_sp) if idx != 0]
+        stopTimes = {}
+        for idx, stLine in enumerate(reader_st):
+            if idx == 0:
+                continue
+            stopTimes[(stLine[3],stLine[0])] = StopTime(stLine[0],stLine[1],stLine[2],stLine[3])
         
-        stopTimes = []
-        stopTimes = [StopTime(stLine[3],stLine[0]) for idx, stLine in enumerate(reader_st) if idx != 0]
-        
-        trips = []
-        trips = [Trip(trLine[2],trLine[0]) for idx, trLine in enumerate(reader_tr) if idx != 0]
-        
-        routes = []
-        routes = [Route(rtLine[0],rtLine[2]) for idx, rtLine in enumerate(reader_rt) if idx != 0]
-        
+        trips = {}
+        for idx, trLine in enumerate(reader_tr):
+            if idx == 0:
+                continue
+            trips[(trLine[0],trLine[2])] = Trip(trLine[0],trLine[2],trLine[3])
+
+        routes = {}
+        for idx, rtLine in enumerate(reader_rt):
+            if idx == 0:
+                continue
+            routes[(rtLine[0])] = Route(rtLine[0],rtLine[2],rtLine[3])
+
+        # function returns 4 dictionaries
         return stops, stopTimes, trips, routes
 
-# funkcia vráti 4 listy
-stops, stopTimes, trips, routes = createObjects()
 # funkcia potrebuje ako argumenty názvy súborov, alebo ich napíšte priamo do "with" časti
+stops, stopTimes, trips, routes = createObjects()
+
+# príklad, ako získať atribút
+print((trips[('L991', '991_4357_230213')]).refToSTs)

806365e05e99947b22060dcda148ffcec044b4eb
Martin Šíma <sima.pacov@seznam.cz>
2023-03-20 22:40:59 +0100

    přidání docstringů

--- StopSegment.py
+++ StopSegment.py
@@ -2,11 +2,30 @@ from math import floor
 from prettytable import PrettyTable
 
 class StopSegment:
+    '''
+    Class respesenting section between 2 adjacent stops
+    ...
+    
+    Atributes:
+    ---------
+    start:
+        name of the staring stop
+    finnish :
+        name of the ending stop 
+    counter :
+        number of related trips
+    trips :
+        array of objects of related trips
 
+    Methods
+    ------
+    __add_trip(trip):
+        adding object od related trip to the array self.__trips
+    '''
     def __init__(self, start, finnish) -> None:
         self.__start = start
         self.__finnish = finnish
-        self.__counter = 0
+        self.__counter = 1
         self.__trips = []
     
     @property
@@ -29,21 +48,43 @@ class StopSegment:
     def trips(self):
         return self.__trips
     
-    def add_trip(self, trip) -> None:
+    def __add_trip(self, trip) -> None:
+        '''
+        adding related trip to array of trips
+        
+        Parameters:
+        ----------
+        trip :
+            object of class Trip, trip which service this segment
+        
+        Return value:
+        -------------
+        None
+        '''
         self.__trips.append(trip)
         self.counter = self.counter + 1
 
 # Chtělo by to nějakou třídu pro listy a metody create_segments()) a busiest()
 # stopsSegmets - pole objektů třídy StopSegment
-stopSegments = []       # pole s objekty stopSegment
-
-def __ss_exist(self, start, finnish) -> StopSegment|bool:
-    for ss in stopSegments:
-        if  ss.start == start and ss.finnish == finnish:      
-            return ss
-    return False
+stopSegments = {}       
+'''
+Dictionary  key:    stopSegment_id (joined stop_id of start and stop_id of finnish)
+            value:  object of StopSegment
+'''
 
 def __merge_sort(self, array : list) -> list:
+    '''
+        sorting array of objects of stopSegments using merge sort
+        
+        Parameters:
+        ----------
+        array :
+            array of object (class StopSegment) to sort
+        
+        Return value:
+        -------------
+        None
+    '''
     if len(array) > 1:
         sorted_1 = self.__merge_sort(array[:floor(len(array)/2)])
         sorted_2 = self.__merge_sort(array[floor(len(array)/2):])
@@ -67,16 +108,37 @@ def __merge_sort(self, array : list) -> list:
     return array 
 
 def __sort_Segments(self) -> None:
+    '''
+        method for calling method merge soft
+        
+        Parameters:
+        -----------
+        None
 
+        Return value:
+        -------------
+        None
+        '''
     self.__merge_sort(self.stopSegments)
     
-def busiest(self, date : date) -> None:
+def busiest(self) -> None:
+    '''
+        calculating and printing five busiest stopSegments
+        
+        Parameters:
+        ----------
+        None
+
+        Return value:
+        -------------
+        None
+    '''
     self.__sort_Segments()
     table = PrettyTable(['Start', 'Finnish', 'Number of trips', 'Routes'])
     for item in stopSegments[:5]:
         table.add_row([item.start, item.to, item.count, ''])
         routes=[]
-        for trip in item.trips.values():
+        for trip in item.trips:
             routes.append(trip.reftoRoute.name)
         set_routes = set(routes)
         routes = list(set_routes)
@@ -86,15 +148,40 @@ def busiest(self, date : date) -> None:
     print(table)
     
 def create_StopSegments(self) -> None:
-    for item in trips.values():              # pole objektů třídy Trip
-        for idx in range(len(item.reftoSTs) - 1):
-            result = __ss_exist(item.reftoSTs[idx].reftoStop.name, item.reftoSTs[idx + 1].reftoStop.name)
-            if not result:
-                segment = StopSegment(item.reftoSTs[idx].reftoStop.name, item.reftoSTs[idx + 1].reftoStop.name)
-                segment.trips.append(item)
-                stopSegments.append(segment)
+    '''
+        method for creating objects of class StopSegment
+        
+        Parameters:
+        ----------
+        None
+        
+        Return value:
+        -------------
+        None
+    '''
+    current_trip_id = None
+    start = None
+    finish = None
+    start_id = None
+    finish_id = None
+    for (key, stopTime) in stopTimesT:
+        if current_trip_id != key:
+            current_trip_id = key
+            start = stopTime.refToStop.name
+            start_id = stopTime.refToStop.id
+        else:
+            finish = stopTime.refToStop.name
+            finish_id = stopTime.refToStop.id
+            Segment = stopSegments.get(start_id + finish_id)
+            if not Segment:
+                stopSegments[start_id + finish_id] = StopSegment(start, finish)
+                stopSegments[start_id + finish_id].trips.append(stopTime.refToTrip)
             else:
-                result.trips.append(item)
+                Segment.counter += 1
+                Segment.trips.append(stopTime.refToTrip)
+            start = stopTime.refToStop.name
+            start_id = stopTime.refToStop.id
+        current_trip_id = key
 
 # Sekvence volání metod: 
 #self.Create_stopSegments()
